0a944784fa4c37610c6f48004076606c13bda23c
Brandan Quinn <bquinn2@ramapo.edu>
2018-10-30 14:07:41 -0400

    Linked game activity to starting screen. Created layout for game activity.

--- app/src/main/java/com/brandanquinn/casino/casino/StartScreen.java
+++ app/src/main/java/com/brandanquinn/casino/casino/StartScreen.java
@@ -1,13 +1,34 @@
 package com.brandanquinn.casino.casino;
 
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 
 public class StartScreen extends AppCompatActivity {
+
+    private Button newGame;
+
+    private View.OnClickListener newGameOnClickListener = new View.OnClickListener() {
+        @Override
+        public void onClick(View v) {
+            newGameClicked();
+        }
+    };
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_start_screen);
+
+        newGame = findViewById(R.id.newgame);
+        newGame.setOnClickListener(newGameOnClickListener);
+    }
+
+    public void newGameClicked() {
+        newGame.setText("Button clicked.");
+        Intent i = new Intent(getApplicationContext(), GameScreen.class);
+        startActivity(i);
     }
 }
